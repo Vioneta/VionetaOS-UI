c0ba98c9cfad8e3627c583e32f608e5089e56aca
charith <nikhuge@gmail.com>
2024-05-20 07:20:39 +0530

    done

--- main/src/components/syncthing/SyncBlock.vue
+++ main/src/components/syncthing/SyncBlock.vue
@@ -5,14 +5,14 @@
 		<div class="blur-background"></div>
 		<div class="wuji-content _box is-flex is-flex-direction-column">
 			<!-- Init State Start -->
-			<h6 class="title is-4 mb-0 has-text-white is-flex-shrink-0">{{ $t(`Sync your data`) }}</h6>
+			<h6 class="title is-4 mb-0 has-text-white is-flex-shrink-0">{{ $t(`Create meaningfull inside from data`) }}</h6>
 			<div class="is-flex is-align-items-center is-flex-grow-1 _notice-content-text">
 				<div class="info ">
 					<div class="des two-line is-size-14px">
-						{{ $t(`Use Syncthing to sync your files between multiple devices.`) }}
+						{{ $t(`VInsight is Vioneta's Advanced Analytical engine `) }}
 					</div>
 				</div>
-				<b-image :src="require('@/assets/img/syncthing/sync.svg')" class="is-80x80"></b-image>
+				<b-image :src="require('@/assets/img/syncthing/icon.png')" class="is-80x80"></b-image>
 			</div>
 			<div class="buttons is-flex is-flex-shrink-0 is-flex-direction-row-reverse">
 				<b-button class="mb-0" rounded size="is-small" type="is-primary" @click="openSyncPanel">{{
